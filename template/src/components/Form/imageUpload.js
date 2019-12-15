b4549de74f3bcfe161da7247f7b56abac3039b31
Bruendermiester <jonbruenderman@hotmail.com>
2019-12-14 20:58:49 -0800

    Updating to background image instead of img tag

--- template/src/components/Form/imageUpload.js
+++ template/src/components/Form/imageUpload.js
@@ -7,21 +7,25 @@ class renderImageField  extends React.Component{
   render() {
 
     let newContent = Object.assign([], this.props.content); 
-    let displaySrc = newContent[this.props.index].src || '';
+    let displaySrc = {
+      backgroundImage: `url( ${newContent[this.props.index].src} )`
+    }
     return(
-     <div>
-        <img id={`image-${this.props.index}`} src={displaySrc}></img>
-        <div>
+     <div className="image_box">
+        <div id={`image-${this.props.index}`} className="display_image" style={ displaySrc } ></div>
+        <div className="image_size">Size {newContent[this.props.index].file ? ((newContent[this.props.index].file.size/(1024*1024)).toFixed(2)) : 0} - MB </div>
+        <div className="box">
             <input
                 type='file'
+                className="upload_btn"
                 accept='.jpg, .png, .jpeg'
                 onChange={(e) => {
                     if (e.target.files && e.target.files[0]) {
                       let reader = new FileReader();
                       newContent[this.props.index].file = e.target.files[0];
-                      reader.onload = (e) => {
-                        document.getElementById(`image-${this.props.index}`).src = e.target.result;
-                        newContent[this.props.index].src = e.target.result;
+                      reader.onload = () => {
+                        displaySrc = {backgroundImage: `url( ${ reader.result } )`};
+                        newContent[this.props.index].src = reader.result;
                         this.props.updateBlogBodyContent(newContent);
                       };
                       reader.readAsDataURL(e.target.files[0]);
