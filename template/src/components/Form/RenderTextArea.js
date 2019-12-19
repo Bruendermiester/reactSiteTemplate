6acdaebd3d9f3e98ba449d1c01273933a26c1b0f
Bruendermiester <jonbruenderman@hotmail.com>
2019-12-18 23:04:03 -0800

    replacing textarea with contenteditable fields

--- template/src/components/Form/RenderTextArea.js
+++ template/src/components/Form/RenderTextArea.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { updateBlogBodyContent } from '../../actions';
+import ContentEditable from 'react-contenteditable';
+import sanitizeHtml from "sanitize-html";
 
 class RenderTextArea extends React.Component {
+
 
     render() {
         let newContent = Object.assign([], this.props.content);
@@ -17,24 +20,64 @@ class RenderTextArea extends React.Component {
             return wordCount;
             }
             else {
-                return 0
+                return 0;
             }
+        }
+        let sanitizeConf = {
+            allowedTags: ["b", "i", "em", "strong", "a", "p", "h1"],
+            allowedAttributes: { a: ["href"] }
+        };        
+        let sanitize = () => {
+            newContent[this.props.index].text = sanitizeHtml(newContent[this.props.index].text, sanitizeConf)
+            this.props.updateBlogBodyContent(newContent);
+        };
+
+        let handleChange = (e) => {
+            newContent[this.props.index].text = e.target.value;
+            this.props.updateBlogBodyContent(newContent);
         }
 
         return (
             <div>
-                <textarea type='text' value={textValue}  onChange={(e) => {
-                    newContent[this.props.index].text = e.target.value;
-                    this.props.updateBlogBodyContent(newContent);
-                    }}/>
+                <ContentEditable
+                    className="editable_textarea"
+                    html={newContent[this.props.index].text} // innerHTML of the editable div
+                    disabled={false}       // use true to disable editing
+                    onChange={handleChange} // handle innerHTML change
+                    onBlur={sanitize}
+                />
                 <div className="info_container">
                     <div className="word_count"> WORDS: <span className="word_count_span"> {getWordCount(newContent[this.props.index].text)}</span></div>
                     <div className="character_count"> CC: <span className="character_count_span"> {newContent[this.props.index].text ? newContent[this.props.index].text.length : 0}</span></div>
+                    <EditButton cmd="italic" />
+                    <EditButton cmd="bold" />
+                    <EditButton cmd="formatBlock" arg="h1" name="heading" />
+                    <EditButton
+                        className="action_btn"
+                        cmd="createLink"
+                        arg="https://github.com/lovasoa/react-contenteditable"
+                        name="hyperlink"
+                    />
                 </div>
             </div>
         );
     }
 }
+
+function EditButton(props) {
+    return (
+      <button
+        className="action_btn"
+        key={props.cmd}
+        onMouseDown={evt => {
+          evt.preventDefault(); // Avoids loosing focus from the editable area
+          document.execCommand(props.cmd, false, props.arg); // Send the command to the browser
+        }}
+      >
+        {props.name || props.cmd}
+      </button>
+    );
+  }
 
 const mapStateToProps = (state) => {
     return { content: state.content };
